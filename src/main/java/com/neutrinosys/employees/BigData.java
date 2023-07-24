857bd9c86778bab9080a071c810d84a42bbf8a55
Jonathan McNeal <hyperstar89@gmail.com>
2023-07-24 04:11:37 -0500

    Starting Lesson 176 now

--- src/main/java/com/neutrinosys/employees/BigData.java
+++ src/main/java/com/neutrinosys/employees/BigData.java
@@ -3,8 +3,11 @@ package com.neutrinosys.employees;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-import static java.util.stream.Collectors.summingLong;
+import static java.util.stream.Collectors.*;
 
 public class BigData {
 
@@ -12,14 +15,16 @@ public class BigData {
     public static void main(String[] args) {
         try {
             long startTime = System.currentTimeMillis();
-            long count = Files.lines(Path.of("C:/Users/hyper/Downloads/Hr5m/Hr5m.csv")).parallel()
+            Map<String, String> count = Files.lines(Path.of("C:/Users/hyper/Downloads/Hr5m/Hr5m.csv")).parallel()
                     .skip(1)
 //                    .limit(10)
                     .map(s-> s.split(","))
                     .map(a -> new Person(a[2], a[4], Long.parseLong(a[25]), a[32]))
-                    .collect(summingLong(Person::salary));
+                    .collect(groupingBy(Person::state, TreeMap::new,
+                            collectingAndThen(summingLong(Person::salary), s-> String.format("$%,d.00%n", s))));
             long endTime = System.currentTimeMillis();
-            System.out.printf("$%,d.00%n",count);
+//            System.out.printf("$%,d.00%n",count);
+            System.out.println(count);
             System.out.println(endTime - startTime);
         } catch (IOException e) {
             throw new RuntimeException(e);
