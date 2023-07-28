38e73fc1fa4374823a59382037715b066d0227da
JonathanMcNeal <93370368+JonathanMcNeal@users.noreply.github.com>
2023-07-27 21:28:53 -0500

    Section 10 completed

--- src/main/java/com/neutrinosys/employees/BigData.java
+++ src/main/java/com/neutrinosys/employees/BigData.java
@@ -1,27 +1,29 @@
 package com.neutrinosys.employees;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import static java.util.stream.Collectors.*;
 
 public class BigData {
 
-    record Person (String firstName, String lastName, long salary, String state){}
+    record Person (String firstName, String lastName, BigDecimal salary, String state, char gender){}
     public static void main(String[] args) {
         try {
             long startTime = System.currentTimeMillis();
-            Map<String, String> count = Files.lines(Path.of("C:/Users/hyper/Downloads/Hr5m/Hr5m.csv")).parallel()
+//            Map<String, String> count =
+           // Map<String(state). Map<char(gender), String(formatted-salary)>>
+            Map<Boolean, Map<String, Long>> count = Files.lines(Path.of("C:/Users/hyper/Downloads/Hr5m/Hr5m.csv")).parallel()
                     .skip(1)
-//                    .limit(10)
-                    .map(s-> s.split(","))
-                    .map(a -> new Person(a[2], a[4], Long.parseLong(a[25]), a[32]))
-                    .collect(groupingBy(Person::state, TreeMap::new,
-                            collectingAndThen(summingLong(Person::salary), s-> String.format("$%,d.00%n", s))));
+//                    .limit(200)
+                    .map(s -> s.split(","))
+                    .map(a -> new Person(a[2], a[4], new BigDecimal(a[25]), a[32], a[5].strip().charAt(0)))
+                    .collect(partitioningBy(p -> p.gender()=='F',
+                            groupingBy(Person::state, counting())));
+//                    .forEach((state, salary) -> System.out.printf("%s -> %s%n", state, salary));
             long endTime = System.currentTimeMillis();
 //            System.out.printf("$%,d.00%n",count);
             System.out.println(count);
